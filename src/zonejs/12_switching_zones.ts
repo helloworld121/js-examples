d375136300502a0b8a541a90d68b60ef3eb2b616
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-01-13 12:43:45 +0100

    zonejs example for switching zones

---
+++ src/zonejs/12_switching_zones.ts
@@ -0,0 +1,20 @@
+import 'zone.js';
+
+const rootZone = Zone.current;
+
+const zoneA = rootZone.fork({name: 'A'});
+const zoneAC = zoneA.fork({name: 'AC'});
+const zoneB = rootZone.fork({name: 'B'});
+
+function c() {
+    console.log('funcation c, running in zone:', Zone.current.name);  // AC
+}
+function b() {
+    console.log('funcation b, running in zone:', Zone.current.name);  // B
+    zoneAC.run(c);
+}
+function a() {
+    console.log('funcation a, running in zone:', Zone.current.name);  // AC
+    zoneB.run(b);
+}
+zoneAC.run(a);
